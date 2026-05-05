8ae390968660c3316ccedc76567f83b163c4deb4
镐赞 <haozanlawyer@gmail.com>
2026-05-05 23:34:58 +0800

    fix(js): remove deleted controllers from stimulus registry to fix build

--- app/javascript/controllers/index.ts
+++ app/javascript/controllers/index.ts
@@ -5,7 +5,6 @@ import DropdownController from "./dropdown_controller"
 import SdkIntegrationController from "./sdk_integration_controller"
 import ClipboardController from "./clipboard_controller"
 import TomSelectController from "./tom_select_controller"
-import FlatpickrController from "./flatpickr_controller"
 import SystemMonitorController from "./system_monitor_controller"
 import FlashController from "./flash_controller"
 import WemdEditorController from "./wemd_editor_controller"
@@ -14,7 +13,6 @@ import ImageUploadController from "./image_upload_controller"
 import HistoryPanelController from "./history_panel_controller"
 import RedirectController from "./redirect_controller"
 import ThemeSyncController from "./theme_sync_controller"
-import CalendarController from "./calendar_controller"
 
 const application = Application.start()
 
@@ -23,7 +21,6 @@ application.register("dropdown", DropdownController)
 application.register("sdk-integration", SdkIntegrationController)
 application.register("clipboard", ClipboardController)
 application.register("tom-select", TomSelectController)
-application.register("flatpickr", FlatpickrController)
 application.register("system-monitor", SystemMonitorController)
 application.register("flash", FlashController)
 application.register("wemd-editor", WemdEditorController)
@@ -32,6 +29,5 @@ application.register("image-upload", ImageUploadController)
 application.register("history-panel", HistoryPanelController)
 application.register("redirect", RedirectController)
 application.register("theme-sync", ThemeSyncController)
-application.register("calendar", CalendarController)
 
 window.Stimulus = application
